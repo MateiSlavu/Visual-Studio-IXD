689dde3b10a5b7e0ffb4d730e9024bc2e7e62af5
Matei Slavu <kingonebravo1@gmail.com>
2024-04-04 21:49:06 +0300

    prime number

--- PrimeNumber/Form1.cs
+++ PrimeNumber/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PrimeNumber
@@ -17,38 +11,50 @@ namespace PrimeNumber
             InitializeComponent();
         }
 
+        private void PrimeNumberTextBox_TextChanged(object sender, EventArgs e)
+        {
+            // Resetăm culoarea de fundal la implicit
+            this.BackColor = SystemColors.Control;
+        }
+
         private void CHECKButton_Click(object sender, EventArgs e)
         {
             int result = 0;
             bool isNumber = Int32.TryParse(PrimeNumberTextBox.Text, out result);
             if (isNumber)
             {
-                TrueOrFalseLabel.Text = "true";
+                if (result > 1)
+                {
+                    bool isPrime = true;
+                    for (int i = 2; i <= result / 2; i++)
+                    {
+                        if (result % i == 0)
+                        {
+                            isPrime = false;
+                            break;
+                        }
+                    }
+                    if (isPrime)
+                    {
+                        TrueOrFalseLabel.Text = "true";
+                        this.BackColor = Color.Green;
+                    }
+                    else
+                    {
+                        TrueOrFalseLabel.Text = "false";
+                        this.BackColor = Color.Red;
+                    }
+                }
+               
             }
             else
-                TrueOrFalseLabel.Text = "false";
-            if (isNumber)
             {
-                bool isPrime = true;
-                for (int i = 2; i <= result / 2; i++)
-                {
-                    if (result % i == 0)
-                        isPrime = false;
-                }
-                if (isPrime)
-                {
-                    this.BackColor = Color.Green;
-                }
-                else
-                {
-                    this.BackColor = Color.Red;
-                }
-
-
+                TrueOrFalseLabel.Text = "false";
+                this.BackColor = Color.Yellow; 
+                MessageBox.Show("Nu s-a introdus un număr întreg.");
             }
-            else { MessageBox.Show("nu s a introdus un numar intreg."); }
-
         }
     }
-} 
+}
+
 
